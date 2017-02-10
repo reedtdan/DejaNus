49159689dea8f0077cf160410c297482f8ada8fe
Daniel Reed <reedtdan@gmail.com>
2017-02-10 10:55:35 -0500

    Changed from Eclipse IDE to Netbeans 8.2
    Netbeans and Eclipse organize projects in different ways. We may as
    well all use the same IDE.

--- src/dejanus/DejaNus.java
+++ src/dejanus/DejaNus.java
@@ -1,8 +1,6 @@
 public class DejaNus {
 	public static void main( String[] args ) {
 		Prompt a = new Prompt();
-		Query b = new Query();
-		Filter c = new Filter();
 		Ranker d = new Ranker();
 		Display e = new Display();
 	}
